5648b2060ba0410882e811f6f5498274e3c5a4be
Shohith Kumar K <shohithkumar22@gmail.com>
2026-09-05 11:05:29 +0530

    built appliction

---
+++ test_app.py
@@ -0,0 +1,19 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.get_json()["message"] == "CI/CD pipeline application is running!"
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.get_json()["status"] == "healthy"
